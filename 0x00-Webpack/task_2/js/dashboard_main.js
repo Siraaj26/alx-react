987144d63fcf0f0b4ce2db9ea493733ec00730d4
Siraaj Jeftha <63967985+Siraaj26@users.noreply.github.com>
2023-04-17 21:46:22 +0200

    Update dashboard_main.js

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,15 +1,14 @@
 import '../css/main.css';
-const $ = require('jquery');
-const _ = require('lodash');
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 let counter = 0;
-
-$("<div id='logo'> </div>").appendTo('body');
-$("<p>Holberton Dashboard</p>").appendTo('body');
-$("<p>Dashboard data for the students</p>").appendTo('body');
-$("<button>Click here to get started</button>").appendTo('body');
-$("<p id='count'></p>").appendTo('body');
-$("<p>Copyright - Holberton School</p>").appendTo('body');
 
 function updateCounter() {
   counter++;
